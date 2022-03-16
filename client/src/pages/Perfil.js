42910463cd000ef307f15e2de3d132de3e7e8653
Kappelmeister <sa.guillen00@gmail.com>
2022-03-17 00:51:12 +0100

    botones perfil

--- client/src/pages/Perfil.js
+++ client/src/pages/Perfil.js
@@ -8,10 +8,10 @@ function Perfil() {
     const cookie = new Cookies()
 
     const [usuario, auth] = InfoAuth("obtieneinfo")
-    
+
     useEffect(() => {
         if (auth === true) {
-        } else if (auth === false ) {
+        } else if (auth === false) {
             window.location.href = "/"
         }
     }, [auth])
@@ -32,23 +32,23 @@ function Perfil() {
 
     return (
         <div>
-        <div className="Frame-3628"> 
-            <div id="crearColiving">
-            <button type="button" className="Btn-Default-Home"  onClick={crearColiving}><span className="Texto-Blanco">Registrar Coliving</span></button>
+            <div className="Frame-3628">
+                <div id="crearColiving">
+                    <button type="button" className="Btn-Default-Home" onClick={crearColiving}><span className="Texto-Blanco-perfil">Registrar Coliving</span></button>
+                </div>
+                <br />
+                <div id="modificarPerfil">
+                    <button type="button" className="Btn-Default-Home" onClick={irATest}><span className="Texto-Blanco-perfil">Modificar Perfil</span></button>
+                </div>
+                <br />
+                <div id="eliminarPerfil">
+                    <button type="button" className="Btn-Default-Home"><span className="Texto-Blanco-perfil">Eliminar Perfil</span></button>
+                </div>
+                <div id="cerrarSesión">
+                    <button type="button" className="Btn-Default-Home" onClick={logout}><span className="Texto-Blanco-perfil">Cerrar Sesión</span></button>
+                </div>
             </div>
-            <br />
-            <div id="modificarPerfil">
-                <button type="button" className="Btn-Default-Home" onClick={irATest}>Modificar Perfil</button>
-            </div>
-            <br />
-            <div id="eliminarPerfil">
-            <button type="button" className="Btn-Default-Home">Eliminar Perfil</button>
-            </div>
-            <div id="cerrarSesión">
-            <button type="button"className="Btn-Default-Home" onClick={logout}>Cerrar Sesión</button>
-            </div>
-        </div>
-        <NavBar />
+            <NavBar />
         </div>
     )
 }
